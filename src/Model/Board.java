c49996541892d8298f0f2db35b7a1bc78067c5ab
Iquecs2005 <iquecarvalho2005@gmail.com>
2025-10-08 21:26:30 -0300

    Board and Player classes

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,5 +1,41 @@
 package Model;
 
-class Board {
+import java.util.LinkedList;
+import java.util.List;
 
+class Board 
+{
+	private List<Space> terrainsList = new LinkedList<Space>();
+	
+	public Board() 
+	{
+		//Sets terrainList
+		terrainsList.add(new Space());
+	}
+	
+	public Space MovePlayer(Player player, int amount) 
+	{
+		Space playerCurrentSpace = player.GetCurrentSpace();
+		int playerLocation = terrainsList.indexOf(playerCurrentSpace);
+		
+		if (playerLocation == -1) 
+			return null;
+		
+		int spaceIndex = playerLocation + amount;
+		if (spaceIndex > terrainsList.size()) 
+		{
+			//Bank.Transfer(player, 200)
+			spaceIndex = spaceIndex % terrainsList.size();
+		}
+		Space newSpace = terrainsList.get(spaceIndex);
+		
+		player.SetCurrentSpace(newSpace);
+		
+		return newSpace;
+	}
+	
+	public Space GetStartSpace() 
+	{
+		return terrainsList.get(0);
+	}
 }
--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,5 +1,79 @@
 package Model;
 
-class Player {
+import java.util.List;
+import java.util.LinkedList;
 
+class Player 
+{
+	private String color;
+	private float money;
+	private Space currentSpace;
+	private List<Space> ownedSpaces = new LinkedList<Space>();
+	
+	public Player(String color, float money, Space currentSpace) 
+	{
+		this.color = color;
+		this.money = money;
+		this.currentSpace = currentSpace;
+	}
+	
+	public boolean BuySpace()
+	{
+		//if (Bank.Pay(this, currentSpace.price))
+		//{
+		//  currentSpace.SetOwner(this);
+		//	ownedSpaces.add(currentSpace);
+		//	return true;
+		//}
+		
+		return false;
+	}
+	
+	public boolean SellSpace(Space soldSpace) 
+	{
+		int spaceIndex = ownedSpaces.indexOf(soldSpace);
+		
+		if (spaceIndex == -1) 
+			return false;
+		
+		//Bank.Transfer(this, currentSpace.sellValue);
+		//currentSpace.SetOwner(null);
+		
+		return true;
+	}
+	
+	public float GetMoney() 
+	{
+		return money;
+	}
+	
+	public void SetMoney(float money) 
+	{
+		this.money = money;
+	}
+	
+	public String GetColor() 
+	{
+		return color;
+	}
+
+	public void SetColor(String color) 
+	{
+		this.color = color;
+	}
+	
+	public Space GetCurrentSpace() 
+	{
+		return currentSpace;
+	}
+	
+	public void SetCurrentSpace(Space currentSpace) 
+	{
+		this.currentSpace = currentSpace;
+	}
+	
+	public List<Space> GetOwnedSpaces() 
+	{
+		return ownedSpaces;
+	}
 }
