7cc0493cc545114ae21b73be56d755a42ac4766c
jomigmonte <jomigmonte@gmail.com>
2025-10-08 23:31:54 -0300

    BANCO!!!!!!!

--- src/Model/Bank.java
+++ src/Model/Bank.java
@@ -1,5 +1,32 @@
 package Model;
 
 class Bank {
-
+	private float money;
+	
+	public Bank(float money)
+	{
+		this.money = money;
+	}
+	
+	public void GiveMoney(float amount, Player player)
+	{
+		this.money -= amount;
+		float playerMoney = player.GetMoney();
+		player.SetMoney(playerMoney + amount);
+	}
+	
+	public void TakeMoney(float amount, Player player)
+	{
+		this.money += amount;
+		float playerMoney = player.GetMoney();
+		player.SetMoney(playerMoney - amount);
+	}
+	
+	public void TransferMoney(float amount, Player playerGet, Player playerGive)
+	{
+		float getMoney = playerGet.GetMoney();
+		float giveMoney = playerGive.GetMoney();
+		playerGet.SetMoney(getMoney + amount);
+		playerGive.SetMoney(giveMoney - amount);
+	}
 }
