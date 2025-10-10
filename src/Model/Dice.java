0ded570b13e92741827df00d5264d677e1cd4a8e
EnzoGCastro <enzoguerrieridecastro@gmail.com>
2025-10-10 15:51:53 -0300

    Make dice class

--- src/Model/Dice.java
+++ src/Model/Dice.java
@@ -1,5 +1,33 @@
 package Model;
 
 class Dice {
-
+	
+	private int sides;
+	
+	public Dice (int sides) {
+		this.sides = sides;
+	}
+	
+	
+	
+	
+	public int roll () {
+		
+		return (int)(Math.random()*(this.sides) + 1);
+		
+	}
+	
+	public Vector<Integer> roll (int nrolls) {
+		
+		Vector<Integer> result = new Vector<>();
+		
+		for (int i = 0; i < nrolls; i++) {
+			
+			result.add(roll());
+			
+		}
+		
+		return result;
+	}
+	
 }
