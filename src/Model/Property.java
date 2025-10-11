c8cc2ba0e9efc2d06da6162acf711f9727bca796
jomigmonte <jomigmonte@gmail.com>
2025-10-11 02:06:28 -0300

    mojo dojo casa house e cálculo do valor do espaço

--- src/Model/Property.java
+++ src/Model/Property.java
@@ -1,26 +1,40 @@
 package Model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 class Property extends Space {
 	
 	private int price;
 	private int rent;
+	private int baseRent;
 	private Player owner;
+	private Hotel hotel;
+	private House house;
 	
-	public Property(String name, int price, int rent) {
+	public Property(String name, int price, int rent, Hotel hotel, House house) {
 		super(name);
 		this.price = price;
 		this.rent = rent;
+		this.baseRent = rent;
 		this.owner = null;
+		this.hotel = hotel;
+		this.house = house;
 	}
 	
 	public void setOwner(Player owner) {
 		this.owner = owner;
+	}	
+	
+	public boolean onLand(Player p) {	
+		return (p == owner);
 	}
 	
-	public boolean onLand(Player p) {
-		
-		
-		
+	private void UpdateRent()
+	{
+		int hotelValue = this.hotel.GetAmount() * this.hotel.GetRent();
+		int houseValue = this.house.GetAmount() * this.house.GetRent();
+		this.rent = this.baseRent + hotelValue + houseValue;
 	}
 	
 	
