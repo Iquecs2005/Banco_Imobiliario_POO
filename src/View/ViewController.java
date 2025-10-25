27b10bf1f2f233d3012b6cb4ea78e4536a976106
Iquecs2005 <iquecarvalho2005@gmail.com>
2025-10-25 18:40:45 -0300

    Menu Principal

--- src/View/ViewController.java
+++ src/View/ViewController.java
@@ -1,16 +1,18 @@
 package View;
 
+import javax.swing.*;
+
 public class ViewController 
 {
+	static JButton b1 = new JButton("New Game");
+	static JButton b2 = new JButton("Load Game");
+	
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		WindowFrame window = new WindowFrame(500, 500);
 		window.setTitle("Banco Imobiliario");
 		window.setVisible(true);
-		
-		InitialPanel frame = new InitialPanel();
-		window.getContentPane().add(frame);
 	}
 
 }
--- src/View/WindowFrame.java
+++ src/View/WindowFrame.java
@@ -5,6 +5,11 @@ import javax.swing.*;
 
 class WindowFrame extends JFrame
 {
+	JPanel panel;
+	
+	static JButton b1 = new JButton("New Game");
+	static JButton b2 = new JButton("Load Game");
+	
 	public WindowFrame(int startWidth, int startHeight) 
 	{
 		Toolkit tk = Toolkit.getDefaultToolkit();
@@ -13,10 +18,45 @@ class WindowFrame extends JFrame
 		int screenWidth = screenSize.width;
 		int screenHeight = screenSize.height;
 
-		int x = (screenWidth - startWidth) / 2;
-		int y = (screenHeight - startWidth) / 2;
+		int x = screenWidth / 2 - startWidth / 2;
+		int y = screenHeight / 2 - startHeight / 2;
 		
 		setBounds(x, y, startWidth, startHeight);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		panel = new InitialPanel(startWidth, startHeight);
+		getContentPane().add(panel);
+		
+		PositionButton(b1, startWidth / 2 - 5, startHeight / 2 - 25);
+		panel.add(b1);
+		PositionButton(b2, startWidth / 2 - 5, startHeight / 2 + 25);
+		panel.add(b2);
+	}
+	
+	public void paintComponent(Graphics g) 
+	{
+		super.paintComponents(g);
+		
+		Dimension panelSize = panel.getSize();
+		
+		g.drawString("Banco Imobiliario", 200, 200);
+	}
+	
+	private void PositionButton(JButton button, int x, int y) 
+	{
+		Dimension buttonSize = button.getPreferredSize();
+		
+		int buttonX;
+		int buttonY;
+		int sizeX;
+		int sizeY;
+		
+		sizeX = buttonSize.width;
+		sizeY = buttonSize.height;
+		buttonX = sizeX / 2;
+		buttonY = sizeY / 2;
+		
+		button.setFocusable(false);
+		button.setBounds(new Rectangle(x - buttonX, y - buttonY, sizeX, sizeY));
 	}
 }
