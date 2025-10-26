54a340ea4e1564214820b6af016da527614e5a33
jomigmonte <jomigmonte@gmail.com>
2025-10-26 13:23:58 -0300

    cartas pokémon

--- src/Model/Card.java
+++ src/Model/Card.java
@@ -1,18 +1,96 @@
 package Model;
 
+import java.util.List;
+
 class Card 
 {
-	private String id;
-	
-	public Card(String id) 
+	public enum CardType
 	{
-		this.id = id;
+		Receive, Lose, ReceiveFromAll, GoToBeginning, GoToJail, LeaveJail
 	}
 	
+	private static Bank bank;
+	private static Board board;
+	private static Jail jail;
+	private Player owner;
+	private String id = "0";
+	private int amount = 0;
+	private CardType type;
 	
+	public Card(CardType type) 
+	{
+		this.type = type;
+	}
+	
+	public Card(int amount, CardType type) 
+	{
+		this.amount = amount;
+		this.type = type;
+	}
+	
+	public static void SetStaticVariables(Bank bank, Board board, Jail jail)
+	{
+		Card.bank = bank;
+		Card.board = board;
+		Card.jail = jail;
+	}
 	
 	public String GetCardId() 
 	{
 		return id;
+	}
+	
+	public void SetOwner(Player owner)
+	{
+		this.owner = owner;
+	}
+	
+	private void ReceiveAll(List<Player> players)
+	{
+		for (Player player: players)
+		{
+			if (this.owner != player)
+			{
+				player.TransferMoney(owner, 50);
+			}
+		}
+	}
+	
+	private void GoToBeginning(Board board)
+	{
+		Space startSpace = board.GetStartSpace();
+		owner.SetCurrentSpace(startSpace);
+		bank.TransferMoney(owner, 200);
+	}
+	
+	private void GoToJail(Jail jail)
+	{
+		jail.sendToJail(owner);
+	}
+	
+	private void LeaveJail()
+	{
+		//player.hasJailCard = true;
+	}
+	
+	public void UseCard(List<Player> players)
+	{
+		switch (this.type)
+		{
+		case Receive:
+			bank.TransferMoney(owner, amount);
+		case Lose:
+			owner.TransferMoney(bank, amount);
+		case ReceiveFromAll:
+			ReceiveAll(players);
+		case GoToBeginning:
+			GoToBeginning(board);
+		case GoToJail:
+			GoToJail(jail);
+		case LeaveJail:
+			LeaveJail();
+		default:
+			break;
+		}
 	}
 }
---
+++ src/Model/Deck.java
@@ -0,0 +1,58 @@
+package Model;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Deck {
+	private HashMap<Integer, Card> deck = new HashMap<Integer, Card>();
+	private List<Player> players = new LinkedList<Player>();
+	
+	public Deck(List<Player> players, Bank bank, Board board, Jail jail)
+	{
+		this.players = players;
+		Card.SetStaticVariables(bank, board, jail);
+		CreateDeck();
+	}
+	
+	private void CreateDeck()
+	{
+		deck.put(1, new Card(25, Card.CardType.Receive));
+		deck.put(2, new Card(150, Card.CardType.Receive));
+		deck.put(3, new Card(80, Card.CardType.Receive));
+		deck.put(4, new Card(200, Card.CardType.Receive));
+		deck.put(5, new Card(50, Card.CardType.Receive));
+		deck.put(6, new Card(50, Card.CardType.Receive));
+		deck.put(7, new Card(100, Card.CardType.Receive));
+		deck.put(8, new Card(100, Card.CardType.Receive));
+		deck.put(9, new Card(Card.CardType.LeaveJail));
+		deck.put(10, new Card(Card.CardType.GoToBeginning));
+		deck.put(11, new Card(Card.CardType.ReceiveFromAll));
+		deck.put(12, new Card(45, Card.CardType.Receive));
+		deck.put(13, new Card(100, Card.CardType.Receive));
+		deck.put(14, new Card(100, Card.CardType.Receive));
+		deck.put(15, new Card(20, Card.CardType.Receive));
+		deck.put(16, new Card(15, Card.CardType.Lose));
+		deck.put(17, new Card(25, Card.CardType.Lose));
+		deck.put(18, new Card(45, Card.CardType.Lose));
+		deck.put(19, new Card(30, Card.CardType.Lose));
+		deck.put(20, new Card(100, Card.CardType.Lose));
+		deck.put(21, new Card(100, Card.CardType.Lose));
+		deck.put(22, new Card(40, Card.CardType.Lose));
+		deck.put(23, new Card(Card.CardType.GoToJail));
+		deck.put(24, new Card(30, Card.CardType.Lose));
+		deck.put(25, new Card(50, Card.CardType.Lose));
+		deck.put(26, new Card(25, Card.CardType.Lose));
+		deck.put(27, new Card(30, Card.CardType.Lose));
+		deck.put(28, new Card(45, Card.CardType.Lose));
+		deck.put(29, new Card(50, Card.CardType.Lose));
+		deck.put(30, new Card(50, Card.CardType.Lose));
+	}
+	
+	public void GetCard(Player player)
+	{
+		Card card = deck.get((int)(Math.random()*(30) + 1));
+		card.SetOwner(player);
+		card.UseCard(players);
+	}
+}
