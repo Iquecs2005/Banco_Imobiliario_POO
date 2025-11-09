9e61dc1e43d34e05035355504cf85d7b127034f3
EnzoGCastro <enzoguerrieridecastro@gmail.com>
2025-11-09 11:46:36 -0300

    make properties hashmap

--- src/View/BoardPanel.java
+++ src/View/BoardPanel.java
@@ -77,4 +77,89 @@ public class BoardPanel extends BasePanel {
         	i++;
         }
     }
+    
+    private Map<String, BufferedImage> propertyCards() {
+    	Map<String, BufferedImage> propertyCards = new HashMap<String, BufferedImage>();
+    	
+    	try {
+    		BufferedImage currentPropertyCard;
+    		
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_9_de_julho.png"));
+    		propertyCards.put("av_9_de_julho", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_atlantica.png"));
+    		propertyCards.put("av_atlantica", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_brasil.png"));
+    		propertyCards.put("av_brasil", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_brigadero_faria_lima.png"));
+    		propertyCards.put("av_brigadero_faria_lima", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_europa.png"));
+    		propertyCards.put("av_europa", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_nossa_s..de_copacabana.png"));
+    		propertyCards.put("av_nossa_s..de_copacabana", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_pacaembu.png"));
+    		propertyCards.put("av_pacaembu", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_paulista.png"));
+    		propertyCards.put("av_paulista", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_presidente_vargas.png"));
+    		propertyCards.put("av_presidente_vargas", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_reboucas.png"));
+    		propertyCards.put("av_reboucas", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/av_vieira_souto.png"));
+    		propertyCards.put("av_vieira_souto", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/Botafogo.png"));
+    		propertyCards.put("Botafogo", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/Brooklin.png"));
+    		propertyCards.put("Brooklin", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/Copacabana.png"));
+    		propertyCards.put("Copacabana", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/Flamengo.png"));
+    		propertyCards.put("Flamengo", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/Interlagos.png"));
+    		propertyCards.put("Interlagos", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/Ipanema.png"));
+    		propertyCards.put("Ipanema", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/jardim.europa.png"));
+    		propertyCards.put("jardim.europa", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/jardim_paulista.png"));
+    		propertyCards.put("jardim_paulista", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/Leblon.png"));
+    		propertyCards.put("Leblon", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/Morumbi.png"));
+    		propertyCards.put("Morumbi", currentPropertyCard);
+
+    		currentPropertyCard = ImageIO.read(getClass().getResource("/resources/cards/properties/rua_augusta.png"));
+    		propertyCards.put("rua_augusta", currentPropertyCard);
+    		
+    	}
+    	
+    	catch(IOException e) {
+    		System.err.println("Image file not found");
+            e.printStackTrace();
+    	}
+    	
+    	return propertyCards;
+    }
+    
+    
+    
 }
